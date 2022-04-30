3e170379a2daf42eebe9ec011478d815ab8fd38d
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-04-30 16:34:12 -0400

    feat: Setup Menu component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,44 @@
 import './Menu.scss';
 
-const Menu = () => {
-  return <div>Menu</div>;
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+
+const Menu = ({ active, setActive }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    setActive(false);
+  }, [location]);
+
+  return (
+    <ul className={`menu ${active ? 'active' : ''}`}>
+      <li>
+        <Link to="/">
+          H<span>ome</span>
+        </Link>
+      </li>
+      <li>
+        <Link to="/skills">
+          S<span>kills</span>
+        </Link>
+      </li>
+      <li>
+        <Link to="/projects">
+          P<span>rojects</span>
+        </Link>
+      </li>
+      <li>
+        <Link to="/about">
+          A<span>bout</span>
+        </Link>
+      </li>
+      <li>
+        <Link to="/contact">
+          C<span>ontact</span>
+        </Link>
+      </li>
+    </ul>
+  );
 };
 
 export default Menu;
