1d49c584e527368d30fa4f8f18c8185d054ac25f
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-04-30 15:37:06 -0400

    chore: Setup layout of the app

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,7 +1,30 @@
-import './Layout';
+import './Layout.scss';
+
+import { Outlet } from 'react-router-dom';
+
+import { Header, Menu } from '../components';
+import { useState } from 'react';
 
 const Layout = () => {
-  return <div>Layout</div>;
+  const [active, setActive] = useState(false);
+
+  return (
+    <div className={`container ${active ? 'active' : ''}`}>
+      <Header active={active} setActive={setActive} />
+
+      <div className="container__main">
+        <main className="main">
+          <div className="content">
+            <div className="overlay">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </div>
+
+      <Menu />
+    </div>
+  );
 };
 
 export default Layout;
