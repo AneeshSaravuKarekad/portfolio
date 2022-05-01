4c0b70425d2bcd270c9c5a3207167df452442247
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-04-30 23:35:56 -0400

    chore: Wrap the earth component in a canvas

--- src/containers/Hero/Hero.jsx
+++ src/containers/Hero/Hero.jsx
@@ -1,11 +1,17 @@
-import Earth from '../../components/Earth/Earth';
 import './Hero.scss';
+
+import Earth from '../../components/Earth/Earth';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-      <Earth />
+    <div className="hero__container">
+      <Canvas>
+        <Suspense fallback={null}>
+          <Earth />
+        </Suspense>
+      </Canvas>
     </div>
   );
 };
