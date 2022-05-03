82acaaa6067bccc615b7345d1868adcc87131fc9
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-05-03 11:50:18 -0400

    feat: Setup home page text content

---
+++ src/components/HeroContent/HeroContent.jsx
@@ -0,0 +1,32 @@
+import './HeroContent.scss';
+
+import { FaGithub } from 'react-icons/fa';
+import { AiOutlineDownload } from 'react-icons/ai';
+
+const HeroContent = () => {
+  return (
+    <div className="hero-text__container">
+      <div className="hero-text__container-inner">
+        <h1 className="intro">Hi!, I'm Aneesh Saravu Karekad</h1>
+        <span className="sub-intro">
+          A creative Frontend engineer with proficient server side skills
+        </span>
+
+        <div className="cta-container">
+          <button className="cta github">
+            <FaGithub />
+            Github
+          </button>
+          <button className="cta resume">
+            <AiOutlineDownload size={20} />
+            <a href="../../../resume.pdf" download="AneeshSaravuKarekad">
+              Resume
+            </a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroContent;
