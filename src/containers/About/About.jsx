767be9a216fc54823adcc8c74c90eff3de16bfaf
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-05-04 12:03:38 -0400

    feat: Setup about section

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,7 +1,94 @@
 import './About.scss';
 
+import Icon from '@mdi/react';
+import {
+  mdiSpeedometer,
+  mdiCellphoneLink,
+  mdiRocket,
+  mdiLightbulbOutline,
+} from '@mdi/js';
+
+import ProfileImage from '../../assets/images/profilePhoto.jpg';
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about-wrapper">
+      <div className="services">
+        <div className="hexagon-wrapper">
+          <div className="hexagon">
+            <Icon path={mdiSpeedometer} color="white" title="fast & reliable" />
+          </div>
+
+          <h3>Fast</h3>
+          <p>
+            Fast load times and lag free interactions are my highest priority
+          </p>
+        </div>
+
+        <div className="hexagon-wrapper">
+          <div className="hexagon">
+            <Icon
+              path={mdiCellphoneLink}
+              color="white"
+              title="fast & reliable"
+            />
+          </div>
+
+          <h3>Responsive</h3>
+          <p>My layouts will work on any device, big or small</p>
+        </div>
+
+        <div className="hexagon-wrapper">
+          <div className="hexagon">
+            <Icon
+              path={mdiLightbulbOutline}
+              color="white"
+              title="responsive design"
+            />
+          </div>
+
+          <h3>Intuitive</h3>
+          <p>Strong preference for easy to use, intuitive UX/UI</p>
+        </div>
+
+        <div className="hexagon-wrapper">
+          <div className="hexagon">
+            <Icon path={mdiRocket} color="white" title="fast & reliable" />
+          </div>
+
+          <h3>Dynamic</h3>
+          <p>
+            Websites don't have to be static, I love making pages come to life
+          </p>
+        </div>
+      </div>
+
+      <div className="about" id="About">
+        <div className="about__content">
+          <div className="profile-image hexagon">
+            {/* <img
+              src={ProfileImage}
+              className="clip-image"
+              alt="Portrait image of Aneesh"
+            /> */}
+          </div>
+          <div>
+            <h2 className="about__content-heading">Me, Myself & I</h2>
+            <p className="about__content-paragraph">
+              I'm a Full stack Javascript developer located in Canada. I focus
+              on giving users a creative, intuitive and dynamic experience.
+            </p>
+            <p className="paragraph2">
+              Well-organized person, problem, solver, independent developer with
+              high attention to detail.
+            </p>
+          </div>
+        </div>
+
+        <div className="about__skills">sadfaskdjfhsakljdhfjkasdfhajksdfh</div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
