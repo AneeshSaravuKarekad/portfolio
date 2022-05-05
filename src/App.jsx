10a505052b0e5e3bc39213caa7c9e0edec8cc659
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-05-05 16:07:07 -0400

    fix: Routing path

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Hero />} />
 
-          <Route path="projects" element={<Projects />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </div>
