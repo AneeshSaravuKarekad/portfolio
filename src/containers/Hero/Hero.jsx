6a284444318268ff9ec4fd85ae231a081fdc513b
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-05-05 07:04:00 -0400

    fix: Place text content inside canvas

--- src/containers/Hero/Hero.jsx
+++ src/containers/Hero/Hero.jsx
@@ -2,25 +2,34 @@ import './Hero.scss';
 
 import { Canvas } from '@react-three/fiber';
 import { Suspense, useRef } from 'react';
-import { Stars } from '@react-three/drei';
+import { Stars, Html } from '@react-three/drei';
 
 import Earth from '../../components/Earth/Earth';
 import Moon from '../../components/Moon/Moon';
 import HeroContent from '../../components/HeroContent/HeroContent';
+import Contact from '../Contact/Contact';
+import LoadingScreen from '../../components/LoadingScreen/LoadingScreen';
 
 const Hero = () => {
   const group1 = useRef();
 
   return (
     <>
-      <HeroContent />
-
       <div className="hero__container">
         <Canvas
           camera={{ fov: 50, position: [0, 0, 6] }}
           style={{ width: '100vw', height: '100vh' }}
         >
-          <Suspense fallback={null}>
+          <Suspense
+            fallback={
+              <Html>
+                <LoadingScreen />
+              </Html>
+            }
+          >
+            <Html>
+              <HeroContent />
+            </Html>
             <pointLight
               color={'#f6f3ea'}
               position={[2, 0, 6]}
